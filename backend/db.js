abffee330dc197f073d882e3c7d331cf1db60069
Balachandru-ai <balachandru@thestackly.com>
2026-01-05 15:20:51 +0530

    Refactor database column names to lowercase

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,16 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database('./database.db', () => {
+const db = new sqlite3.Database("./database.db", () => {
   console.log("SQLite DB connected");
 });
 
 db.run(`
   CREATE TABLE IF NOT EXISTS deployments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    GIT_REPO TEXT,
-    IMAGE_NAME TEXT,
-    IMAGE_TAG TEXT,
-    CONTAINER_NAME TEXT,
+    git_repo TEXT,
+    image_name TEXT,
+    image_tag TEXT,
+    container_name TEXT,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
   )
 `);
